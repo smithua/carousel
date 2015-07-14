d68d781bd8c6806ac3cdb0050281e4a1d37c5478
smithua <smithuaa@gmail.com>
2015-07-14 10:02:41 +0300

    fix array of images

--- js/array.js
+++ js/array.js
@@ -48,7 +48,7 @@ var items = [
         img6: "img/img6.jpg",
         img7: "img/img7.jpg"
         img8: "img/img8.jpg"
-    }
+    },
     {
         img:  "img/img1.jpg",
         img2: "img/img2.jpg",
